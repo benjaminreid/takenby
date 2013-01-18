642d9da5898e5e007bdaab09430f9a2d6fea634b
Benjamin Reid <benjamin@nouveller.com>
2013-01-18 14:01:18 +0000

    Specify some of the user details in a config file

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+	/*
+	* Fill in your details below
+	* There's also additional links in the ul.social-list 
+	*/
+	
+	$name = "Benjamin Reid";
+	$flickr_username = "nouveller";
+	$twitter_username = "BenjaminReid";
+
+	// single paragraph about yourself
+	$about = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu eros tortor, et pharetra purus. Ut nunc augue, imperdiet sit amet dictum eu, consectetur eget velit.";
--- index.php
+++ index.php
@@ -1,38 +1,43 @@
+<?php include('config.php'); ?>
 <!doctype html>
 <html>
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
-    <title>Taken by Benjamin Reid</title>
+    <title>Taken by <?= $name ?></title>
     <link rel="stylesheet" type="text/css" href="css/styles.css">
   </head>
   <body>
     <header>
       <div class="wrapper">
-        <h1>Taken by <span>Benjamin Reid</span></h1>
+        <h1>Taken by <span><?= $name ?></span></h1>
         <a href="#info" class="sp sp-i-icon i-icon" id="info-toggle">Information</a>
       </div><!-- /.wrapper -->
       <div class="info-box">
         <div class="wrapper">
           <div class="column col-1">
             <h3>About Me</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu eros tortor, et pharetra purus. Ut nunc augue, imperdiet sit amet dictum eu, consectetur eget velit.</p>
+            <p><?= $about ?></p>
           </div><!-- /.column -->
           <div class="column col-2">
-            <h3>Photo sets</h3>
+            <h3>Photo Sets</h3>
             <div id="sets">
               <div class="set-cell last">
-                <a class="view-more" href="http://www.flickr.com/photos/nouveller/sets">View<br />more</a>
+                <a class="view-more" href="http://www.flickr.com/photos/<?= $flickr_username ?>/sets">View<br />more</a>
               </div><!-- /.set-cell .last -->
             </div><!-- /#sets -->
           </div><!-- /.column -->
           <div class="column col-3">
             <h3>You can find me on</h3>
             <ul class="social-list">
-              <li><a href="www.flickr.com/photos/nouveller/">Flickr</a></li>
-              <li><a href="https://twitter.com/#!/BenjaminReid">Twitter</a></li>
-              <li><a href="http://www.benjaminreid.me/">My blog</a></li>
+              <?php if ($flickr_username) : ?>
+              <li><a href="www.flickr.com/photos/<?= $flickr_username ?>/">Flickr</a></li>
+              <?php endif; ?>
+              <?php if ($twitter_username) : ?>
+              <li><a href="https://twitter.com/<?= $twitter_username ?>">Twitter</a></li>
+              <?php endif; ?>
+              <li><a href="http://www.benjaminreid.me/">Blog</a></li>
               <li><a href="http://www.nouveller.com/">Nouveller</a></li>
             </ul>
           </div><!-- /.column -->
@@ -44,7 +49,7 @@
       <div class="container" id="photo-container">
       </div><!-- /#photo-container -->
 
-      <a href="http://www.flickr.com/photos/nouveller/" class="sp sp-more">View more</a>
+      <a href="http://www.flickr.com/photos/<?= $flickr_username ?>/" class="sp sp-more">View more</a>
     </div><!-- /.wrapper -->
     <footer>
       <div class="wrapper">
